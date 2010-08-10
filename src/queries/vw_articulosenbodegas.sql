7551cd81f26051ea44cefc9ed6a087189d10f641
megalucaiv <megalucaiv@dcb628ac-f4cd-4a55-a972-47504abd8c62>
2010-08-10 11:32:40 +0000

    Agregue mi vista vw_articulosenbodegas.

--- src/queries/vw_articulosenbodegas.sql
+++ src/queries/vw_articulosenbodegas.sql
@@ -3,13 +3,12 @@ USE `esquipulasdb`;
 CREATE  OR REPLACE VIEW `esquipulasdb`.`vw_articulosenbodegas` AS
 
 SELECT 
-axd.iddocumento,
-d.idtipodoc,
+axd.idarticulo,
 a.descripcion,
 c.valor*(1+a.ganancia/100) as precio,
 c.valor as costodolar,
 c.valor * tc.tasa as costo,
-IF((axd.unidades > 0 AND kardex.iddocumento IS NULL) AND d.idtipodoc<>27 ,0,axd.unidades),
+SUM(IF((axd.unidades > 0 AND kardex.iddocumento IS NULL) AND d.idtipodoc<>27 ,0,axd.unidades)) as existencia,
 d.idbodega
 FROM `esquipulasdb`.`articulosxdocumento` axd
 JOIN vw_articulosdescritos a ON a.idarticulo=axd.idarticulo
@@ -18,4 +17,5 @@ JOIN tiposcambio tc ON tc.idtc= c.idtc
 JOIN documentos d ON d.iddocumento = axd.iddocumento
 LEFT JOIN docpadrehijos ph ON ph.idpadre = axd.iddocumento
 LEFT JOIN documentos kardex ON ph.idhijo = kardex.iddocumento AND kardex.idtipodoc=27
+GROUP BY axd.idarticulo,d.idbodega
 ;
