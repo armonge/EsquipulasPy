7c4e5dd73858256fe7ebc5558793fe7c031c53e2
megalucaiv <megalucaiv@dcb628ac-f4cd-4a55-a972-47504abd8c62>
2010-08-10 11:18:35 +0000

    POR FIN SIRVIO LA CONSULTA QUE TOMA EN CUENTA EL KARDEX, y como ya no necesito la otra vista, entonces la borre

--- src/queries/vw_articulosenbodegas.sql
+++ src/queries/vw_articulosenbodegas.sql
@@ -1,19 +1,21 @@
 USE `esquipulasdb`;
 
 CREATE  OR REPLACE VIEW `esquipulasdb`.`vw_articulosenbodegas` AS
-Select 
-axd.idarticulo,
+
+SELECT 
+axd.iddocumento,
+d.idtipodoc,
 a.descripcion,
 c.valor*(1+a.ganancia/100) as precio,
 c.valor as costodolar,
 c.valor * tc.tasa as costo,
-SUM(IF(axd.unidades>0 AND doc.kardex IS NULL,0,axd.unidades)) as existencia,
-b.idbodega
-FROM articulosxdocumento axd
+IF((axd.unidades > 0 AND kardex.iddocumento IS NULL) AND d.idtipodoc<>27 ,0,axd.unidades),
+d.idbodega
+FROM `esquipulasdb`.`articulosxdocumento` axd
 JOIN vw_articulosdescritos a ON a.idarticulo=axd.idarticulo
-JOIN costosarticulo c ON a.idarticulo=c.idarticulo
+JOIN costosarticulo c ON a.idarticulo=c.idarticulo AND c.activo=1
 JOIN tiposcambio tc ON tc.idtc= c.idtc
-JOIN vw_documentosconkardex doc ON doc.id = axd.iddocumento
-JOIN bodegas b ON b.idbodega=doc.idbodega
-GROUP BY axd.idarticulo,b.idbodega
+JOIN documentos d ON d.iddocumento = axd.iddocumento
+LEFT JOIN docpadrehijos ph ON ph.idpadre = axd.iddocumento
+LEFT JOIN documentos kardex ON ph.idhijo = kardex.iddocumento AND kardex.idtipodoc=27
 ;
--- src/queries/vw_documentosconkardex.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-d.iddocumento as id,
-kardex.iddocumento as kardex,
-d.idbodega
-FROM `esquipulasdb`.`documentos` d
-LEFT JOIN docpadrehijos ph ON ph.idpadre=d.iddocumento
-LEFT JOIN documentos kardex ON kardex.idtipodoc=27 AND kardex.iddocumento=ph.idhijo
-GROUP BY d.iddocumento
